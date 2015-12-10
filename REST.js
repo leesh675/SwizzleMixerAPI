76ac963ac1224b4f6821ffa6d28e6fdfb57b086b
ideabove <leesh675@gmail.com>
2015-12-10 16:10:53 -0600

    first commit

---
+++ REST.js
@@ -0,0 +1,17 @@
+var resource = require("./routes/resource.js");
+
+function REST_ROUTER(router,pool,md5,fs) {
+    var self = this;
+    self.handleRoutes(router,pool,md5,fs);
+
+    // routes
+    new resource().handleRoutes(router,pool,md5,fs);
+}
+
+REST_ROUTER.prototype.handleRoutes= function(router,pool,md5) {
+    router.get("/",function(req,res){
+        res.json({"Message" : "Hello"});
+    })
+}
+
+module.exports = REST_ROUTER;
---
+++ app.js
@@ -0,0 +1,53 @@
+var express     = require("express");
+var bodyParser  = require("body-parser");
+var md5         = require('MD5');
+var rest        = require("./REST.js");
+var fs          = require('fs');
+var app         = express();
+
+function REST(){
+    var self = this;
+    self.configureExpress();
+};
+
+REST.prototype.configureExpress = function(pool) {
+      var self = this;
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
+      var router = express.Router();
+      router.use(function (req, res, next) {
+
+          // Website you wish to allow to connect
+          res.setHeader('Access-Control-Allow-Origin', '*');
+
+          // Request methods you wish to allow
+          res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+          // Request headers you wish to allow
+          res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+          // Set to true if you need the website to include cookies in the requests sent
+          // to the API (e.g. in case you use sessions)
+          res.setHeader('Access-Control-Allow-Credentials', true);
+
+          // Pass to next layer of middleware
+          next();
+      });
+
+      app.use('/', router);
+      var rest_router = new rest(router,pool,md5,fs);
+      self.startServer();
+}
+
+REST.prototype.startServer = function() {
+      app.listen(3000,function(){
+          console.log("All right ! I am alive at Port 3000.");
+      });
+}
+
+REST.prototype.stop = function(err) {
+    console.log("ISSUE WITH MYSQL \n" + err);
+    process.exit(1);
+}
+
+new REST();
